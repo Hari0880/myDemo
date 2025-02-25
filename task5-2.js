9c958a13f982852c0412ae3ec1aca904118cfbb9
Hari0880 <harinarapongu08@gmail.com>
2025-02-25 14:15:13 +0530

    Add files via upload

--- task5-2.js
+++ task5-2.js
@@ -1,9 +1,30 @@
-const os = require('os')
+const http = require('http')
 
-let time = os.uptime()
-let minutes = Math.floor((time/60))
-let hours = Math.floor(minutes/60)
-let days = Math.floor(hours/24)
-console.log(minutes%60)
-console.log(hours)
-console.log(days)
+let server = http.createServer((req,res)=>{
+    try{
+        if(req.method === 'GET'){
+            if(req.url==='/home'){
+            res.writeHead(200,{"content-type":"plain/text"});
+            res.end('Welcome Home page ');
+            }
+        else if(req.url==='/about'){
+            res.writeHead(200,{"content-type":"plain/text"});
+            res.end('This About the About page');
+
+        }else if(req.url==='/contact'){
+           res.writeHead(200,{"content-type":"plain/text"});
+           res.end("contact us")
+        }
+        }else{
+            res.writeHead(404,{"content-type":"plain/text"});
+            res.end('page Not Found');
+        }
+    }catch(error){
+        res.writeHead(500,{"content-type":"plain/text"});
+        res.end('method not found')
+    }
+});
+
+ server.listen(9000,()=>{
+    console.log("this page is runs at the 9000");
+ })
